a7a8561333e22f7cc504be3d554ea5ddcdf07d20
Martijn Schotel <martijn.schotel@eneco.com>
2019-06-29 11:17:38 +0200

    Basic autorization

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+import withFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+interface AutherizationProps {
+  user?: any;
+  signOut?: any;
+  signInWithGoogle?: any;
+}
+
+const App: React.FC<AutherizationProps> = ({
+  user,
+  signOut,
+  signInWithGoogle,
+}) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and be awesome
-        </a>
+        {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+        {user ? (
+          <button onClick={signOut}>Sign out</button>
+        ) : (
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+        )}
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBivZiHRkBW0YmarL-1-GQ90EKN6MXKntU',
+  authDomain: 'jvbproductions-b7cb7.firebaseapp.com',
+  databaseURL: 'https://jvbproductions-b7cb7.firebaseio.com',
+  projectId: 'jvbproductions-b7cb7',
+  storageBucket: '',
+  messagingSenderId: '808887170743',
+  appId: '1:808887170743:web:7c0a19f831680717',
+};
+
+export default firebaseConfig;
