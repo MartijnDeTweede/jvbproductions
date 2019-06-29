901fd9379a349b2bb545c957556beaeaad44a726
Martijn Schotel <martijn.schotel@eneco.com>
2019-06-29 10:46:40 +0200

    my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and be awesome
         </a>
       </header>
     </div>
